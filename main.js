90ebd835e580a229768453db3abd66fc843daf85
DEFECTNIY <145113063+DEFECTNIY@users.noreply.github.com>
2023-11-27 20:01:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// 1 задание 
+for (let i = 2; i <= 12; i += 2) {
+  console.log(i);
+}
+
+// 2 задание
+for(let i=10; i>=1; i--){
+  console.log(i)
+}
+
+// 3 задание
+let i = 0
+while( i < 12){
+    i +=2
+  console.log(i)
+}
+
+let a=10
+while(a>0){
+  a--
+  console.log(a)
+}
